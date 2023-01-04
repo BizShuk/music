147d610711af8aab7d255b9c5a3a8fd3f91e9b6e
shuk <biz.shuk@gmail.com>
2023-01-04 19:18:58 +0800

    Update

--- download/ytmp4-dl.sh
+++ download/ytmp4-dl.sh
@@ -4,9 +4,9 @@
 # brew install youtube-dl
 # brew install ffmpeg/avconv # for mp3
 
-echo Downloading $1
+echo Downloading "$1"
 
 # youtube-dl -f 'bestvideo[ext=mp4][height<=720]+bestaudio[ext=m4a][height<=720]/best[height<=720]' $1
-youtube-dl -f 'bestvideo[height<=720][ext=mp4]+bestaudio/best[height<=720][ext=m4a]' $1
+# youtube-dl -f 'bestvideo[height<=720][ext=mp4]+bestaudio/best[height<=720][ext=m4a]"' "$1
 
-# youtube-dl -f mp4 $1
+youtube-dl -f mp4 "$1"
